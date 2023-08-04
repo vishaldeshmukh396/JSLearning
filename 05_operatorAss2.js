4c8d2e137845b8d1e66b36066043cd3b2cc43169
vishaldeshmukh396 <138035204+vishaldeshmukh396@users.noreply.github.com>
2023-08-04 21:59:00 +0530

    Operators in javascript

---
+++ 05_operatorAss2.js
@@ -0,0 +1,84 @@
+var  greaterNumber= function(argu1,argu2){
+    console.log(`--------------------------Step-1-----------------------`);
+    var argu1=10;
+    var argu2=-10;
+var result=argu1>argu2;
+console.log(`Number 10 is Greater than Number -10 ==>${result}`);
+
+
+}
+greaterNumber(10,-10);
+var  greaterNumber= function(argu1,argu2){
+    console.log(`--------------------------Step-2-----------------------`);
+    var argu1=800;
+    var argu2=899;
+var result=argu1>argu2;
+console.log(`Number 800 is Greater than Number 899 ==>${result}`);
+
+
+}
+greaterNumber(800,899);
+console.log(`-------------------------------------Step-3------------------------------------`);
+var EvenOrOddNum=function(num){
+    var num = 29;
+    
+
+    var result = num%2==0 ? "EVEN" : "ODD";
+    
+    console.log(`Number 29 is A==>>${result}`); 
+
+}
+EvenOrOddNum(29);
+
+var EvenOrOddNum=function(num){
+    var num = 44;
+    
+
+    var result = num%2==0 ? "EVEN" : "ODD";
+    
+    console.log(`Number 44 is A==>>${result}`); 
+
+}
+EvenOrOddNum(44);
+
+var EvenOrOddNum=function(num){
+    var num = 0;
+    
+
+    var result = num%2==0 ? "EVEN" : "ODD";
+    
+    console.log(`Number 0 is A==>>${result}`); 
+
+}
+EvenOrOddNum(0);
+var EvenOrOddNum=function(num){
+    var num = 101;
+    
+
+    var result = num%2==0 ? "EVEN" : "ODD";
+    
+    console.log(`Number 101 is A==>>${result}`); 
+
+}
+EvenOrOddNum(101);
+console.log(`-------------------------------------------------Step-04-------------------------------------`);
+function squareOfWordLength(argu1) {
+    console.log(`---------------------------------------------------------------------------------`);
+var result=argu1;
+console.log(`  Word is==>> ${result}`);
+var res=result.length;
+console.log(`Length of given Word Is==>> ${res}`);
+var num = res;
+    
+
+var result = num%2==0 ? "EVEN" : "ODD";
+
+console.log(`Number IS ==>>${result}`); 
+
+
+}
+squareOfWordLength("JavaScript");
+squareOfWordLength("Google");
+squareOfWordLength ("Developer");
+
+
---
+++ 05_operatorAssinment.js
@@ -0,0 +1,30 @@
+console.log(`-------------------------------------Assignment-01-------------------------------------`);
+function squareOfWordLength(argu1) {
+    console.log(`---------------------------------------------------------------------------------`);
+var result=argu1;
+console.log(` 1st Word is==>> ${result}`);
+var res=result.length;
+console.log(`Length of given Word Is==>> ${res}`);
+    var ress=res*res;
+    console.log(`Square Of Given Word Length Is==>>${ress}`);
+
+}
+squareOfWordLength("JavaScript");
+squareOfWordLength("Google Chrome");
+squareOfWordLength ("Developer Smart");
+
+console.log(`---------------------------------------Step-02---------------------------`);
+var res=function(){
+var output="I am Angular Developer"
+console.log(`Given String Is==>>${output}`);
+var out=output.length;
+console.log(`Length of String Is==>>${out}`);
+var ans=out/4;
+console.log(`String Divded By Total Num Of Word==>> ${ans}`);
+var answer=out*4;
+console.log(`String Value Multiplyed By Words ==>>${answer}`);
+
+
+
+}
+res()
